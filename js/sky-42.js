31130b3a8b2625ba48e2de191b30e55296ebbda8
SKY-42 <SKY-42@SOL.AT>
2017-07-03 10:29:17 +0200

    formspree

--- js/sky-42.js
+++ js/sky-42.js
@@ -87,3 +87,26 @@ $('.nav-link').on('click',function() {
     ga('send', 'pageview');
 
 
+    var $contactForm = $('#contactform');
+    $contactForm.submit(function(e) {
+      e.preventDefault();
+      $.ajax({
+        url: '//formspree.io/sky-42@sol.at',
+        method: 'POST',
+        data: $(this).serialize(),
+        dataType: 'json',
+        beforeSend: function() {
+          $contactForm.append('<div class="alert alert--loading">Sending message…</div>');
+        },
+        success: function(data) {
+          $contactForm.find('.alert--loading').hide();
+          $contactForm.append('<div class="alert alert--success">Message sent!</div>');
+        },
+        error: function(err) {
+          $contactForm.find('.alert--loading').hide();
+          $contactForm.append('<div class="alert alert--error">Ops, there was an error.</div>');
+        }
+      });
+    });
+
+
