c46ce3cedbdf4dd5cc40d8dd6e827e3c2e8a654e
SKY-42 <SKY-42@SOL.AT>
2017-07-14 13:58:56 +0200

    Translation + Form + Landing

--- js/sky-42.js
+++ js/sky-42.js
@@ -20,123 +20,101 @@ $('.nav-link').on('click',function() {
 });
 
 //translation
-    function translate(){
-      $(".navbar").i18n();
-      $("#landing").i18n();
-      $("#about").i18n();
-      $("#pilot").i18n();
-      $("#prices").i18n();
-      $("#contact").i18n();
-      $("#customer").i18n();
-      $("#booking").i18n();
+function translate(){
+  $(".navbar").i18n();
+  $("#landing").i18n();
+  $("#about").i18n();
+  $("#pilot").i18n();
+  $("#prices").i18n();
+  $("#contact").i18n();
+  $("#customer").i18n();
+  $("#booking").i18n();
+}
 
-    }
+var userLang = navigator.language || navigator.userLanguage; 
 
-    var userLang = navigator.language || navigator.userLanguage; 
+if (userLang == "de"){
+  changeToDE();
+}else{
+  changeToEN();
+}
 
-    if (userLang == "de"){
-      changeToDE();
-    }else{
-      changeToEN();
-    }
+//init i18next
+var option = {
+  lng               : userLang,
+  fallbackLng       : "en",
+  resGetPath        : "locales/__lng__/__ns__.json"
+};
 
-    //init i18next
-    var option = {
-      lng               : userLang,
-      fallbackLng       : "en",
-      resGetPath        : "locales/__lng__/__ns__.json"
-    };
+i18n.init(option, function(err, t) {
+    translate();
+});
 
-    i18n.init(option, function(err, t) {
-        translate();
+
+function changeToDE() {
+    i18n.setLng('de', function(err ,t){
+      console.log("set to de");
+      userLang = "de";
+      translate();
     });
 
+    $(".en").removeClass("active");
+    $(".de").addClass("active");
 
-    function changeToDE() {
-        i18n.setLng('de', function(err ,t){
-          console.log("set to de");
-          userLang = "de";
-          translate();
-        });
+}
 
-        $(".en").removeClass("active");
-        $(".de").addClass("active");
-
-    }
-
-    function changeToEN() {
-        i18n.setLng('en', function(err ,t){
-          userLang = "en";
-          console.log("set to en");
-          translate();
-        });
-
-        $(".de").removeClass("active");
-        $(".en").addClass("active");
-    }
-
-    var contactForm =  document.getElementById('contactform');
-    contactForm.setAttribute('action', '//formspree.io/' + 'sky' + '-' + '42' + '@' + 'sol' + '.' + 'at');
-
-    //Google Analytics
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-    ga('create', 'UA-70464984-1', 'auto');
-    ga('send', 'pageview');
-
-
-
-    //Form Validation and Formspree
-    //var contactForm = $('#contactform');
-    $("contactform").validate({
-
-      //submit if valid
-      submitHandler: function(contactForm){
-        $contactForm.submit(function(e) {
-          alert("submit");
-          e.preventDefault();
-
-          //Name
-          var name = $("#fname").val();
-          //Email
-          var email = $("#email").val();
-          //Phone
-          var phonne = $("#phone").val();
-          //Subject
-          var subject = $("#subject").val();
-          //Message
-          var message = "debug";//$("#booking-message").val();
-          alert(message);
-          //Next
-          var next = $("#next").val();
-
-          $.ajax({
-            url: '//formspree.io/sky-42@sol.at',
-            method: 'POST',
-            data: {
-              _replyto:email,
-              _subject:subject,
-              _next:next,
-              language:userLang
-            },
-            dataType: 'json',
-            beforeSend: function() {
-              $contactForm.append('<div class="alert alert--loading">Sending message…</div>');
-            },
-            success: function(data) {
-              $contactForm.find('.alert--loading').hide();
-              $contactForm.append('<div class="alert alert--success">Message sent!</div>');
-            },
-            error: function(err) {
-              $contactForm.find('.alert--loading').hide();
-              $contactForm.append('<div class="alert alert--error">Ops, there was an error.</div>');
-            }
-          });
-        });
-      }
+function changeToEN() {
+    i18n.setLng('en', function(err ,t){
+      userLang = "en";
+      console.log("set to en");
+      translate();
     });
+
+    $(".de").removeClass("active");
+    $(".en").addClass("active");
+}
+
+var contactForm =  document.getElementById('contactform');
+contactForm.setAttribute('action', '//formspree.io/' + 'sky' + '-' + '42' + '@' + 'sol' + '.' + 'at');
+
+//Google Analytics
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-70464984-1', 'auto');
+ga('send', 'pageview');
+
+//Form Validation and Formspree
+$("contactform").validate({
+
+  //submit if valid
+  submitHandler: function(contactForm){
+    $contactForm.submit(function(e) {
+      alert("submit");
+      e.preventDefault();
+
+      //Email
+      var email = $("#email").val();
+      //Subject
+      var subject = $("#subject").val();
+      alert(message);
+      //Next
+      var next = $("#next").val();
+
+      $.ajax({
+        url: '//formspree.io/sky-42@sol.at',
+        method: 'POST',
+        data: {
+          _replyto:email,
+          _subject:subject,
+          _next:next,
+        },
+        dataType: 'json'
+      });
+    });
+  }
+});
 
     
 
